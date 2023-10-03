9dd78073398ef58cd0562da943d32f3e8736eac0
easy297 <saybosyn.77@gmail.com>
2023-10-03 17:46:11 +0600

    Laying out

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,12 +1,40 @@
-import './App.css'
+import "./styles/header.css";
+import logo from "../assets/logo.svg";
 
-function App() {
-
+function Header() {
   return (
     <>
-      
+      <header className="header">
+        <div className="container">
+          <nav className="nav">
+            <img src={logo} alt="MAPPED" />
+            <ul className="list">
+              <li className="item user">
+                <a href="/" className="link">
+                  ACCOUNT
+                </a>
+              </li>
+              <li className="item world">
+                <a href="/" className="link">
+                  PLANETS
+                </a>
+              </li>
+              <li className="item marker">
+                <a href="/" className="link">
+                  PINS
+                </a>
+              </li>
+              <li className="item search">
+                <a href="/" className="link">
+                  SEARCH
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </header>
     </>
-  )
+  );
 }
 
-export default App
+export default Header;
