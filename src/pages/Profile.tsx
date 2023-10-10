ff3acc388e1044ab9f9074d0170ce628ee0b6fcb
easy297 <saybosyn.77@gmail.com>
2023-10-10 23:54:53 +0600

    Sidebar created

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,14 +1,23 @@
 import Footer from "../layout/Footer/Footer";
 import Header from "../layout/Header/Header";
+import pfp from "../assets/myamoto.jpg";
+
+import style from "./Profile.module.scss";
+import { Sidebar } from "../components/static-components/static-components";
 
 const Profile = () => {
-    return (
-        <>
-        <Header />
-        
-        <Footer />
-        </>
-    );
-}
+  return (
+    <>
+      {/* <Header /> */}
+      <main className={style.wrap}>
+        <div className={style.profile}>
+          <Sidebar />
+          <div className="map-container">MAP</div>
+        </div>
+      </main>
+      <Footer />
+    </>
+  );
+};
 
 export default Profile;
